39331f4236102672e9c337c94433b80ef8e74308
Andrew Lyons <lyons1005@gmail.com.com>
2018-02-23 13:53:49 -0500

    Particle magnet script

---
+++ Unity_TestProject/Assets/Scripts/ParticleMagnet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleMagnet : MonoBehaviour {
+	public GameObject Magnet = null;
+	private Material _mat = null;
+	private ParticleSystemRenderer r = null;
+
+	private void Start () {
+		r = GetComponent <ParticleSystemRenderer> ();
+		_mat = r.material;
+	}
+
+	private void Update () {
+		Vector4 magPos = new Vector4 (Magnet.transform.position.x, Magnet.transform.position.y, Magnet.transform.position.z, 0);
+		_mat.SetVector ("_Mag", magPos);
+	}
+}
