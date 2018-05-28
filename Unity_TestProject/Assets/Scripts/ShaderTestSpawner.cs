c7506a271c1028f1647b13af20cc3b4b714b6c41
Andrew Lyons <lyons1005@gmail.com>
2018-05-28 13:55:24 -0400

    update

--- Unity_TestProject/Assets/Scripts/ShaderTestSpawner.cs
+++ Unity_TestProject/Assets/Scripts/ShaderTestSpawner.cs
@@ -6,21 +6,19 @@ public class ShaderTestSpawner : MonoBehaviour {
 
 	public GameObject UI;
 	public Transform Container;
+	public int width = 5;
+	public int height = 5;
 
 	private void Awake() {
 		ClearScene();
 	}
 
-
-
 	public void SpawnCharacter(GameObject ob) {
-		int width = 5;
-		int height = 5;
 		for (int x = 0; x < width; x++) {
 			for (int y = 0; y < height; y++) {
-				Vector3 location = new Vector3(((float)x - 2.5f) * 3, 0f, ((float)y - 2.5f) * 3);
-				Quaternion rot = Quaternion.Euler(0, Random.Range(0, 360), 0f);
-				Instantiate(ob, location, rot, Container);
+				Vector3 location = new Vector3(((float)x - width*0.5f) * 3, 0.7f, ((float)y - height*0.5f) * 3);
+				Quaternion rotation = Quaternion.Euler(0, Random.Range(0, 360), 0f);
+				Instantiate(ob, location, rotation, Container);
 			}
 		}
 	}
