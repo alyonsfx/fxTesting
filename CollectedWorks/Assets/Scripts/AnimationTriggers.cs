88dc62e926f86ad8f70891b5d31be2e354beb07f
alyonsfx <lyons1005@gmail.com>
2020-10-31 22:14:40 +0000

    stuffs

---
+++ CollectedWorks/Assets/Scripts/AnimationTriggers.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTriggers : MonoBehaviour
+{
+    [SerializeField] private List<ParticleSystem> ParticleSystems = new List<ParticleSystem>();
+
+    public void TriggerParticles(int index)
+    {
+        ParticleSystems[index].Play();
+    }
+}
